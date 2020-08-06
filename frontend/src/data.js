08f96d2eee77d6eee361b9425324318f884dd758
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-08-06 14:01:44 +0600

    Rendered products

---
+++ frontend/src/data.js
@@ -0,0 +1,40 @@
+export default  {
+    products: [
+        {
+            name: 'Slim Shart',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 60,
+            brand: 'Nike',
+            rating: 4.5,
+            numReviews: 5
+        },
+        {
+            name: 'Fit Shart',
+            category: 'Shirts',
+            image: '/images/d1.jpg',
+            price: 50,
+            brand: 'Nike',
+            rating: 4.0,
+            numReviews: 8
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.9,
+            numReviews: 10
+        },
+        {
+            name: 'Best Pants',
+            category: 'Pants',
+            image: '/images/d1.jpg',
+            price: 70,
+            brand: 'Nike',
+            rating: 4.9,
+            numReviews: 15
+        }
+    ]
+}
