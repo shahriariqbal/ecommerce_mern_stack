7a90931535fe88feef673e1c914a371b720e1bce
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-08-07 22:26:03 +0600

    Setup MongoDB, created models & routes

--- backend/server.js
+++ backend/server.js
@@ -1,5 +1,17 @@
 import express from "express";
 import data from "./data";
+import dotenv from "dotenv";
+import config  from "./config";
+import mongoose from "mongoose";
+import userRoute from "./routes/userRoute";
+
+dotenv.config();
+const mongodbUrl = config.MONGODB_URL;
+mongoose.connect(mongodbUrl, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true
+}).catch(error => console.log(error.reason));
 
 const app = express();
 
@@ -13,6 +25,8 @@ app.get("/api/products/:id", (req, res) => {
     res.status(404).send({ msg: "Product Not Found." });
   }
 });
+
+app.use("/api/users", userRoute);
 
 app.get("/api/products", (req, res) => {
   res.send(data.products);
