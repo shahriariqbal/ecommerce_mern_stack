c853cde484fb236c4ab5da76a97d7a274f417749
Shahriar Iqbal <48938313+shahriariqbal079@users.noreply.github.com>
2020-08-07 21:35:40 +0600

    Implemented shopping cart screen

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,9 @@
 import React from "react";
-
 import "./App.css";
-import HomeScreen from './screens/HomeScreen';
-import ProductScreen from './screens/ProductScreen';
-import { BrowserRouter, Route, Link } from 'react-router-dom';
+import HomeScreen from "./screens/HomeScreen";
+import ProductScreen from "./screens/ProductScreen";
+import { BrowserRouter, Route, Link } from "react-router-dom";
+import CartScreen from "./screens/CartScreen";
 
 function App() {
   const openMenu = () => {
@@ -14,52 +14,45 @@ function App() {
   };
 
   return (
-
     <BrowserRouter>
-    <div className="grid-container">
-      <header className="header">
-        <div className="brand">
-          <button onClick={openMenu}> &#9776; </button>
+      <div className="grid-container">
+        <header className="header">
+          <div className="brand">
+            <button onClick={openMenu}> &#9776; </button>
 
-          <Link to="/" > amazona </Link>
-          
-        </div>
-        <div className="header-links">
-          <a href="cart.html"> Cart</a>
-          <a href="signin.html"> Sign In</a>
-        </div>
-      </header>
+            <Link to="/"> amazona </Link>
+          </div>
+          <div className="header-links">
+            <a href="cart.html"> Cart</a>
+            <a href="signin.html"> Sign In</a>
+          </div>
+        </header>
 
-      <aside className="sidebar">
-        <h3>Shopping Categories</h3>
-        <button className="sidebar-close-button" onClick={closeMenu}>
-          X
-        </button>
-        <ul>
-          <li>
-            <a href="index.html"> Pants </a>
-          </li>
-          <li>
-            <a href="index.html"> Shirts </a>
-          </li>
-        </ul>
-      </aside>
+        <aside className="sidebar">
+          <h3>Shopping Categories</h3>
+          <button className="sidebar-close-button" onClick={closeMenu}>
+            X
+          </button>
+          <ul>
+            <li>
+              <a href="index.html"> Pants </a>
+            </li>
+            <li>
+              <a href="index.html"> Shirts </a>
+            </li>
+          </ul>
+        </aside>
 
-      <main className="main">
-        <div className="content">
-          <Route path="/product/:id" component={ProductScreen} />
-          <Route path="/" exact={true} component={HomeScreen} />
-
-
-
-
-
-
-        </div>
-      </main>
-      <footer className="footer">All right reserved</footer>
-    </div>
-    </ BrowserRouter>
+        <main className="main">
+          <div className="content">
+            <Route path="/product/:id" component={ProductScreen} />
+            <Route path="/cart/:id?" component={CartScreen} />
+            <Route path="/" exact={true} component={HomeScreen} />
+          </div>
+        </main>
+        <footer className="footer">All right reserved</footer>
+      </div>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/store.js
+++ frontend/src/store.js
@@ -1,14 +1,26 @@
-import { createStore, combineReducers, compose, applyMiddleware } from 'redux';
-import thunk from 'redux-thunk';
-import { productListReducer, productDetailsReducer } from './reducers/productReducers';
+import { createStore, combineReducers, compose, applyMiddleware } from "redux";
+import thunk from "redux-thunk";
+import Cookie from "js-cookie";
+import {
+  productListReducer,
+  productDetailsReducer,
+} from "./reducers/productReducers";
+import { cartReducer } from "./reducers/cartReducers";
 
-const initialState = {};
+const cartItems = Cookie.getJSON("cartItems") || [];
+
+const initialState = { cart: { cartItems } };
 const reducer = combineReducers({
-    productList: productListReducer,
-    productDetails: productDetailsReducer 
-})
+  productList: productListReducer,
+  productDetails: productDetailsReducer,
+  cart: cartReducer,
+});
 
 const composeEnhancer = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
-const store = createStore(reducer, initialState, composeEnhancer( applyMiddleware(thunk)));
+const store = createStore(
+  reducer,
+  initialState,
+  composeEnhancer(applyMiddleware(thunk))
+);
 
 export default store;
